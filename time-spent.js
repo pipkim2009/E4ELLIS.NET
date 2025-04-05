4f3ef08bd51ee2ab571a7c724d0ba91e158414b6
pipkim2009 <ellishughes09@icloud.com>
2025-04-05 23:03:15 +0100

    added initial project files

---
+++ time-spent.js
@@ -0,0 +1,14 @@
+const randomFunFact = document.getElementById("random-fun-fact")
+
+const startTime = new Date()
+let timeSpent = 0
+
+setInterval(timeSpentRefresh, 1)
+
+function timeSpentRefresh() {
+    timeSpent = Math.round(((new Date) - startTime) / 1000) 
+    randomFunFact.textContent = `You have spent ${timeSpent} seconds on E4Ellis.net.`
+    if (timeSpent > 120) {
+        randomFunFact.textContent += " WOW You need help!"
+    }
+}
