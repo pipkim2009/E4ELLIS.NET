0973d3390448ff12768bd27a5a324db519b3c97b
pipkim2009 <ellishughes09@icloud.com>
2025-04-06 00:16:12 +0100

    set up google ads and fixed the fun fact's styling

--- time-spent.js
+++ time-spent.js
@@ -8,7 +8,7 @@ setInterval(timeSpentRefresh, 1)
 function timeSpentRefresh() {
     timeSpent = Math.round(((new Date) - startTime) / 1000) 
     randomFunFact.textContent = `You have spent ${timeSpent} seconds on E4Ellis.net.`
-    if (timeSpent > 120) {
-        randomFunFact.textContent += " WOW You need help!"
+    if (timeSpent >= 120) {
+        randomFunFact.innerHTML += ` <span class="badge bg-warning">WOW You need help!</span>`
     }
 }
