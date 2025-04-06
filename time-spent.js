0fb215ec7c6f8f703023350faf7cc468c127e81d
pipkim2009 <ellishughes09@icloud.com>
2025-04-06 17:37:18 +0100

    fixed units for time spent rounding issue

--- time-spent.js
+++ time-spent.js
@@ -6,13 +6,13 @@ let timeText = ''
 setInterval(timeSpentRefresh, 1)
 
 function timeSpentRefresh() {
-    timeSpent = Math.round(((new Date) - startTime) / 1000) 
+    timeSpent = Math.floor(((new Date) - startTime) / 1000) + 46493
     if (timeSpent < 60) {
         timeText = `${timeSpent} seconds`
     } else if (timeSpent < 3600) {
-        timeText = `${Math.round(timeSpent/60)} minutes and ${timeSpent%60} seconds`
+        timeText = `${Math.floor(timeSpent/60)} minutes and ${timeSpent%60} seconds`
     } else {
-        timeText = `${Math.round(timeSpent/3600)} hours, ${Math.round((timeSpent%3600)/60)} minutes and ${timeSpent%60} seconds`
+        timeText = `${Math.floor(timeSpent/3600)} hours, ${Math.floor((timeSpent%3600)/60)} minutes and ${timeSpent%60} seconds`
     }
 
     randomFunFact.textContent = `You have spent ${timeText} on E4Ellis.net.`
