e53f0db26fead6554e184e70ed85693f910622a6
pipkim2009 <ellishughes09@icloud.com>
2025-04-06 17:13:47 +0100

    time spent units added

--- time-spent.js
+++ time-spent.js
@@ -2,12 +2,21 @@ const randomFunFact = document.getElementById("random-fun-fact")
 
 const startTime = new Date()
 let timeSpent = 0
-
+let timeText = ''
 setInterval(timeSpentRefresh, 1)
 
 function timeSpentRefresh() {
     timeSpent = Math.round(((new Date) - startTime) / 1000) 
-    randomFunFact.textContent = `You have spent ${timeSpent} seconds on E4Ellis.net.`
+    if (timeSpent < 60) {
+        timeText = `${timeSpent} seconds`
+    } else if (timeSpent < 3600) {
+        timeText = `${Math.round(timeSpent/60)} minutes and ${timeSpent%60} seconds`
+    } else {
+        timeText = `${Math.round(timeSpent/3600)} hours, ${Math.round((timeSpent%3600)/60)} minutes and ${timeSpent%60} seconds`
+    }
+
+    randomFunFact.textContent = `You have spent ${timeText} on E4Ellis.net.`
+
     if (timeSpent >= 120) {
         randomFunFact.innerHTML += ` <span class="badge bg-warning">WOW You need help!</span>`
     }
